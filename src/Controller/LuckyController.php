5266f72f316b8ba8de02790e65eca3304a2f83b0
root <root@DESKTOP-N8RER9V.localdomain>
2022-03-22 22:58:59 +0000

    first commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     * @return Response
+     * @throws Exception
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+//        return new Response(
+//            "<html lang='en'>
+//            <body>Lucky Number: $number</body>
+//            </html>"
+//        );
+    }
+}
